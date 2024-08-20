1bdf919da9e5b733b3cbb6cecf838dae8fa112d5
NazeemK22 <khannazeem321@gmail.com>
2024-08-20 11:24:14 +0530

    Added support for newline delimiters

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -13,3 +13,10 @@ test('should return the sum of two numbers', () => {
     expect(add('1,2')).toBe(3);
 });
 
+test('should return the sum of an unknown amount of numbers', () => {
+    expect(add('1,2,3,4')).toBe(10);
+});
+
+test('should handle newlines as delimiters', () => {
+    expect(add('1\n2,3')).toBe(6);
+});
