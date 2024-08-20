7384f98bdbd1a15e335b44ffea95f32706451c71
NazeemK22 <khannazeem321@gmail.com>
2024-08-20 11:26:00 +0530

    Added handling for negative numbers

--- src/calculator.js
+++ src/calculator.js
@@ -1,14 +1,20 @@
 function add(numbers) {
     if (numbers === '') return 0;
 
-    let delimiter = /[\n,]/; // default delimiters
+    let delimiter = /[\n,]/;
     if (numbers.startsWith('//')) {
         const parts = numbers.split('\n');
-        delimiter = new RegExp(`[${parts[0][2]}]`); // Extract delimiter
+        delimiter = new RegExp(`[${parts[0][2]}]`);
         numbers = parts[1];
     }
 
     const numArray = numbers.split(delimiter).map(Number);
+
+    const negatives = numArray.filter(num => num < 0);
+    if (negatives.length > 0) {
+        throw new Error(`negative numbers not allowed: ${negatives.join(', ')}`);
+    }
+
     return numArray.reduce((sum, num) => sum + num, 0);
 }
 
